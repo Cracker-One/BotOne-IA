198d829603884f2ed8d4305c25275bb6336eb605
ProtoNak <125497965+Cracker-One@users.noreply.github.com>
2023-04-12 15:11:37 +0200

    Update bot_function.py

--- bot_function.py
+++ bot_function.py
@@ -1,10 +1,6 @@
 import datetime
 def response_bot(rsp):
-    if('salut' in rsp):
-        print('Bot : '+str('salut, comment vas-tu ?'))
-    if('quel âge as-tu' in rsp):
-        print('Bot : '+str("J'ai 0 ans"))
-    if('quel est ton nom' in rsp):
-        print('Bot : '+str("Mon nom est BotOne"))
-    if('où vis-tu' in rsp):
-        print('Bot : '+str("Je vis dans ton pc"))
+    if('salut' in rsp): print('Bot : '+str('salut, comment vas-tu ?'))
+    if('quel âge as-tu' in rsp): print('Bot : '+str("J'ai 0 ans"))
+    if('quel est ton nom' in rsp): print('Bot : '+str("Mon nom est BotOne"))
+    if('où vis-tu' in rsp): print('Bot : '+str("Je vis dans ton pc"))
